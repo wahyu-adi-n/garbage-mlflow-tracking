66931265226a273fcc10f33be3e0cec2d68e6f6a
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2022-10-11 14:53:39 +0700

    Delete model: convnext_tiny

--- models/test_model.py
+++ models/test_model.py
@@ -1,9 +1,5 @@
 from GarbageEffNetModelV0 import GarbageEffNetModelV0
-from GarbageConvTinyModelV0 import GarbageConvTinyModelV0
 from torchinfo import summary
 
-# model_0 = GarbageEffNetModelV0()
-# summary(model_0)
-
-model_1 = GarbageConvTinyModelV0()
-summary(model_1)
+model_0 = GarbageEffNetModelV0()
+summary(model_0)
