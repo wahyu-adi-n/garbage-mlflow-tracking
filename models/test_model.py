571aea207eb5785bb5cbbc23842750c227eb9160
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2022-10-11 14:09:40 +0700

    Add new model: convnext_tiny

--- models/test_model.py
+++ models/test_model.py
@@ -1,9 +1,9 @@
-import GarbageEffNetModelV0
-import GarbageRes50NetModelV0
+from GarbageEffNetModelV0 import GarbageEffNetModelV0
+from GarbageConvTinyModelV0 import GarbageConvTinyModelV0
 from torchinfo import summary
 
-model_0 = GarbageEffNetModelV0()
-summary(model_0)
+# model_0 = GarbageEffNetModelV0()
+# summary(model_0)
 
-model_1 = GarbageRes50NetModelV0()
+model_1 = GarbageConvTinyModelV0()
 summary(model_1)
